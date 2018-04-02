c8c8f3bbe22ddda356d3158b6594c3c9d5882352
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-04-02 17:32:03 -0600

    Pass node test to default property completeWord to false

--- src/Node.js
+++ src/Node.js
@@ -1,6 +1,6 @@
 class Node {
-  constructor() {
-
+  constructor(s) {
+    this.completeWord = false;
   }
 }
 
--- test/Node-test.js
+++ test/Node-test.js
@@ -3,7 +3,25 @@ const assert = chai.assert;
 const Node = require('../src/Node.js');
 
 describe('Node', function () {
+  let node;
+
+  beforeEach(function () {
+    node = new Node('c')
+  });
+
   it('should exist', function() {
     assert.isFunction(Node);
   })
+
+  it('should have a defaulted complete word value of false', function() {
+      assert.equal(node.completeWord, false);
+    });
+
+
+  
+  //   it('should have a default popularity value of zero', () => {
+  //     expect(node.popularity).to.equal(0)
+  //   })
+  // })
+  
 })
